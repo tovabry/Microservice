04ab90186a49b87bbb272ab9998776d6ed80ba77
Tova <tova.bryngelsson@live.se>
2025-05-23 11:59:41 +0200

    logger info about client in resourceserver controller

--- resourceserver/src/main/java/se/whispers/resourceserver/HelloController.java
+++ resourceserver/src/main/java/se/whispers/resourceserver/HelloController.java
@@ -1,10 +1,20 @@
 package se.whispers.resourceserver;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+
 @RestController
 public class HelloController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("/private")
     public String privateEndpoint() {
@@ -12,7 +22,15 @@ public class HelloController {
     }
 
     @GetMapping("/public")
-    public String publicEndpoint() {
+    public String publicEndpoint(HttpServletRequest request) {
+        String clientIp = request.getRemoteAddr();
+        int clientPort = request.getRemotePort();
+        List<String> headerNames = Collections.list(request.getHeaderNames());
+
+        logger.info("Client IP: " + clientIp + " Client Port: " + clientPort);
+        logger.info("Request Headers: " + headerNames);
+        logger.info("X-Forwarded-For: " + request.getRemoteHost());
+        logger.info("X-Forwarded-Port: " + request.getRemotePort());
         return "You're visiting the public endpoint of this resource server";
     }
 
