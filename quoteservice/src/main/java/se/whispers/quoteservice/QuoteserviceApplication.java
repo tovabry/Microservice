fbaa39d528a571b082bcbebbe626d4e03f00bdce
Tova <tova.bryngelsson@live.se>
2025-05-26 11:52:32 +0200

    quoteservice setup

---
+++ quoteservice/src/main/java/se/whispers/quoteservice/QuoteserviceApplication.java
@@ -0,0 +1,13 @@
+package se.whispers.quoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuoteserviceApplication.class, args);
+    }
+
+}
---
+++ quoteservice/src/test/java/se/whispers/quoteservice/QuoteserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.whispers.quoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
