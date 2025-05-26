5bbb8cd6f422d80a16bd2705781c6d32a3a7920d
Tova <tova.bryngelsson@live.se>
2025-05-26 11:51:41 +0200

    jokeservice security public and private

--- jokeservice/src/main/java/se/whispers/jokeservice/JokeController.java
+++ jokeservice/src/main/java/se/whispers/jokeservice/JokeController.java
@@ -10,25 +10,20 @@ import java.util.List;
 @RequestMapping("/jokes")
 public class JokeController {
 
-    @GetMapping
-    public String getJoke() {
-        String chickenJoke = "A chicken walked in to a library and said 'book book book, so the librarian got to the bookshelf and got her a book. " +
-                "The next day the chicken came back and said 'book book book', the librarian got her another book. " +
-                "The third day the chicken came back and said 'book book book', the librarian got her a third book. " +
-                "This time the librarian secretly followed the chicken to see what she was doing with all those books. " +
-                "She followed her to a pond where she saw the chicken showing the books to a frog." +
-                "When the chicken showed the book to the frog the frog said 'readit'";
-        String cowJoke = "A cow walked in to a strip club and said 'A class of milk please'. " +
-                "The bartender said 'we don't serve milk here, this is a strip club'. " +
-                "The cow said 'I know, I just wanted to see some udders'";
-        String doveJoke = "A dove flew in to a restaurant and asked 'Do you scramble eggs here?" +
-                "The waiter said 'Yes actually, we do" +
-                "The dove said 'Great, I was looking for a place to have my abortion.'";
+    private final List<String> privateJokes = List.of(
+            "A chicken walked in to a library and said 'book book book, so the librarian got to the bookshelf and got her a book. The next day the chicken came back and said 'book book book', the librarian got her another book. The third day the chicken came back and said 'book book book', the librarian got her a third book. This time the librarian secretly followed the chicken to see what she was doing with all those books. She followed her to a pond where she saw the chicken showing the books to a frog. When the chicken showed the book to the frog the frog said 'readit'",
+                    "A cow walked in to a strip club and said 'A class of milk please'. The bartender said 'we don't serve milk here, this is a strip club'. The cow said 'I know, I just wanted to see some udders'",
+                    "A dove flew in to a restaurant and asked 'Do you scramble eggs here? The waiter said 'Yes actually, we do.' The dove said 'Great, I was looking for a place to have my abortion.'"
+                    );
 
-        List<String> jokes = List.of(chickenJoke, cowJoke, doveJoke);
+    @GetMapping("/private")
+    public String getPrivateJoke() {
+        int randomIndex = (int) (Math.random() * privateJokes.size());
+        return privateJokes.get(randomIndex);
+    }
 
-        int randomIndex = (int) (Math.random() * jokes.size());
-
-        return jokes.get(randomIndex);
+    @GetMapping("/public")
+    public String getPublicJoke() {
+        return "A child walked in to a bar and tried to order a beer. The bartender said 'Sorry kid, you're too young to drink.' The child replied 'That's okay, I'm an undercover cop. I just wanted to see if you would serve me.' The bartender smiled and said 'Haha, that's smart, here's your beer!'";
     }
 }
--- jokeservice/src/main/java/se/whispers/jokeservice/JokeserviceApplication.java
+++ jokeservice/src/main/java/se/whispers/jokeservice/JokeserviceApplication.java
@@ -3,6 +3,7 @@ package se.whispers.jokeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -16,28 +17,26 @@ public class JokeserviceApplication {
         SpringApplication.run(JokeserviceApplication.class, args);
 
     }
-
-
-//    @Bean
-//    SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
-//        http
-//                .cors(cors -> cors.configurationSource(corsConfigurationSource)) //enable CORS
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .requestMatchers("/public").permitAll()
-//                        .requestMatchers("/private").hasAuthority("SCOPE_read_resource")
-//                        .anyRequest().authenticated()
-//                )
-//                //turn on resource server that looks for JWT tokens, with default configuration
-//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
-//        return http.build();
-//    }
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .csrf(csrf -> csrf.disable());
+    SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource)) //enable CORS
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/jokes/public").permitAll()
+                        .requestMatchers("/jokes/private").hasAuthority("SCOPE_read_resource")
+                        .anyRequest().authenticated()
+                )
+                //turn on resource server that looks for JWT tokens, with default configuration
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+//                .csrf(csrf -> csrf.disable());
+//        return http.build();
+//    }
 
 
     @Bean
